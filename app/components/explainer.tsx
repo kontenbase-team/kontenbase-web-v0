f2bd956788799772e9eb12dddec8eb13ded05021
M Haidar Hanif <git@mhaidarhanif.com>
2021-12-19 03:22:40 +0700

    feat: explainer component

---
+++ app/components/explainer.tsx
@@ -0,0 +1,27 @@
+import { FunctionComponent } from 'react'
+
+import { styled } from '~/stitches'
+
+type Explainer = {
+  slug: string
+  title: string
+  description: string
+  direction: string
+  imageUrl: string
+}
+
+interface ExplainerProps {
+  explainer: Explainer
+}
+
+const ExplainerContainer = styled('div', {})
+
+export const Explainer: FunctionComponent<ExplainerProps> = (props) => {
+  return (
+    <ExplainerContainer>
+      <pre>{JSON.stringify(props.explainer, null, 2)}</pre>
+    </ExplainerContainer>
+  )
+}
+
+export default Explainer
