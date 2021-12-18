3f81bfc515bf8a505eb334d56c7e818faeeb74f8
M Haidar Hanif <git@mhaidarhanif.com>
2021-12-18 23:44:56 +0700

    style: button theme styled

--- app/components/button-theme.tsx
+++ app/components/button-theme.tsx
@@ -2,7 +2,19 @@ import { styled } from '~/stitches'
 import { buttonStyles } from '~/components'
 
 export const ButtonToggleThemeContainer = styled('button', {
-  ...buttonStyles,
+  all: 'unset',
+  userSelect: 'none',
+  cursor: 'pointer',
+  height: '$4',
+  px: '$2',
+  fontWeight: '$bold',
+  borderRadius: '$pill',
+  fontSize: '$1',
+  color: '$white',
+  backgroundColor: '$gray9',
+  '&:hover': { backgroundColor: '$gray10' },
+  '&:active': { backgroundColor: '$gray11' },
+  '&:focus': { backgroundColor: '$gray11' },
 })
 
 interface ButtonToggleThemeProps {}
