8f7ce2f84f59b3b44affbfa6a8aab3d86b8dd33d
M Haidar Hanif <git@mhaidarhanif.com>
2021-12-19 04:20:55 +0700

    style+fix: explainer collection and section

--- app/components/explainer.tsx
+++ app/components/explainer.tsx
@@ -1,29 +1,72 @@
 import { FunctionComponent } from 'react'
 
 import { styled } from '~/stitches'
+import { AspectRatio } from '~/components'
 
-type Explainer = {
+export type Explainer = {
   slug: string
   title: string
   description: string
-  direction: string
   imageUrl: string
+  direction: 'left' | 'right' | undefined
 }
 
 interface ExplainerProps {
+  step: number
   explainer: Explainer
 }
 
-const ExplainerContainer = styled('div', {
+const ExplainerSectionContainer = styled('section', {
   width: '$full',
+  display: 'flex',
+  gap: '$10',
+  justifyContent: 'space-between',
+  variants: {
+    direction: {
+      right: { flexDirection: 'row' },
+      left: { flexDirection: 'row-reverse' },
+    },
+  },
 })
 
-export const Explainer: FunctionComponent<ExplainerProps> = (props) => {
+const ExplainerText = styled('div', {
+  maxWidth: '500px',
+  minWidth: '300px',
+})
+
+const ExplainerMedia = styled('div', {
+  border: '0.5rem solid $red3',
+  borderRadius: '0.25rem',
+  width: 720,
+  overflow: 'hidden',
+})
+
+const ExplainerImage = styled('img', {
+  borderRadius: '0.25rem',
+  objectFit: 'cover',
+  width: '100%',
+  height: '100%',
+})
+
+export const ExplainerSection: FunctionComponent<ExplainerProps> = ({
+  step,
+  explainer,
+}) => {
   return (
-    <ExplainerContainer>
-      <pre>{JSON.stringify(props.explainer, null, 2)}</pre>
-    </ExplainerContainer>
+    <ExplainerSectionContainer direction={explainer.direction}>
+      <ExplainerText>
+        <span>Step {step}</span>
+        <h1>{explainer.title}</h1>
+        <p>{explainer.description}</p>
+      </ExplainerText>
+
+      <ExplainerMedia>
+        <AspectRatio.Root ratio={16 / 10}>
+          <ExplainerImage alt={explainer.title} src={explainer.imageUrl} />
+        </AspectRatio.Root>
+      </ExplainerMedia>
+    </ExplainerSectionContainer>
   )
 }
 
-export default Explainer
+export default ExplainerSection
--- app/contents/shared/explainer-steps.tsx
+++ app/contents/shared/explainer-steps.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from 'react'
 
 import { styled } from '~/stitches'
 import { Content, Heading, Explainer } from '~/components'
+import type { Explainer } from '~/components'
 
 interface ExplainerStepsProps {}
 
@@ -13,14 +14,30 @@ const ExplainerStepsContainer = styled('div', {
   '@desktop': { py: '$20' },
 })
 
+const ExplainerCollection = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '$full',
+  mt: '$10',
+  gap: '$20',
+})
+
 export const ExplainerSteps: FunctionComponent<ExplainerStepsProps> = () => {
   return (
     <ExplainerStepsContainer>
       <Content layout="center-vertical">
         <Heading as="h1">How it works?</Heading>
-        {explainerSteps.map((explainer, index) => {
-          return <Explainer explainer={explainer} />
-        })}
+        <ExplainerCollection>
+          {(explainerSteps as Explainer[]).map((explainer, index) => {
+            return (
+              <Explainer
+                key={explainer.slug}
+                step={index + 1}
+                explainer={explainer}
+              />
+            )
+          })}
+        </ExplainerCollection>
       </Content>
     </ExplainerStepsContainer>
   )
