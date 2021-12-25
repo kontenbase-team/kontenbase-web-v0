23b45a0f176b5387a7b9974656b8c165df8bec7a
M Haidar Hanif <git@mhaidarhanif.com>
2021-12-26 06:31:46 +0700

    fix: try to change mdx to md

--- app/post.ts
+++ app/post.ts
@@ -9,16 +9,11 @@ export type Post = {
   title: string
 }
 
-export type Posts = Post[]
-
 export type PostMarkdownAttributes = {
   title: string
 }
 
-// Relative to the server output, not the source!
 const postsPath = path.join(__dirname, '../../posts')
-
-console.log({ postsPath })
 
 function isValidPostAttributes(
   attributes: any
@@ -26,41 +21,41 @@ function isValidPostAttributes(
   return attributes?.title
 }
 
-export async function getPosts() {
-  const dir = await fs.readdir(postsPath)
+type NewPost = {
+  title: string
+  slug: string
+  markdown: string
+}
 
-  return Promise.all(
-    dir.map(async (filename) => {
-      const file = await fs.readFile(path.join(postsPath, filename))
-      const { attributes } = parseFrontMatter(file.toString())
-      invariant(
-        isValidPostAttributes(attributes),
-        `${filename} has bad meta data!`
-      )
-      return {
-        title: attributes.title,
-        slug: filename.replace(/\.mdx$/, ''),
-      }
-    })
-  )
+export async function createPost(post: NewPost) {
+  const md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`
+  await fs.writeFile(path.join(postsPath, post.slug + '.md'), md)
+  return getPost(post.slug)
 }
 
 export async function getPost(slug: string) {
-  const filepath = path.join(postsPath, slug + '.mdx')
+  const filepath = path.join(postsPath, slug + '.md')
   const file = await fs.readFile(filepath)
-
   const { attributes, body } = parseFrontMatter(file.toString())
-
   invariant(
     isValidPostAttributes(attributes),
     `Post ${filepath} is missing attributes`
   )
-
   const html = marked(body)
+  return { slug, html, title: attributes.title }
+}
 
-  return {
-    slug,
-    title: attributes.title,
-    html,
-  }
+export async function getPosts() {
+  const dir = await fs.readdir(postsPath)
+  return Promise.all(
+    dir.map(async (filename) => {
+      const file = await fs.readFile(path.join(postsPath, filename))
+      const { attributes } = parseFrontMatter(file.toString())
+      invariant(isValidPostAttributes(attributes))
+      return {
+        slug: filename.replace(/\.md$/, ''),
+        title: attributes.title,
+      }
+    })
+  )
 }
