f366569a01f6e178643a1e75b9f07b0d3a56ec84
M Haidar Hanif <git@mhaidarhanif.com>
2022-01-20 22:47:19 +0700

    refactor: blog page redirect to new url

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,16 +1,4 @@
 import { LoaderFunction, redirect } from 'remix'
 
-import { Heading } from '~/components'
-
 export const loader: LoaderFunction = async () =>
-  redirect('https://blog.kontenbase.com')
-
-export default function Posts() {
-  return (
-    <div>
-      <Heading as="h1">Kontenbase Blog</Heading>
-
-      {/* <PostsCollection posts={posts} /> */}
-    </div>
-  )
-}
+  redirect('https://a.kontenbase.com/blog')
