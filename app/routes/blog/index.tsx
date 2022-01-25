98a65e4680f2b16e38c446531eb064c95abc251c
M Haidar Hanif <git@mhaidarhanif.com>
2022-01-26 00:42:14 +0700

    feat: blog page not redirect

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,4 +1,39 @@
-import { LoaderFunction, redirect } from 'remix'
+import { json, useLoaderData } from 'remix'
 
-export const loader: LoaderFunction = async () =>
-  redirect('https://a.kontenbase.com/blog')
+import type { MetaFunction, LoaderFunction } from 'remix'
+import { createMeta, ReactGA } from '~/utils'
+
+/**
+ * Meta
+ */
+export const meta: MetaFunction = () =>
+  createMeta({
+    title: 'Kontenbase - Blog',
+    description: 'Blog articles and posts by Kontenbase.',
+    route: 'blog',
+  })
+
+/**
+ * Loader
+ */
+export const loader: LoaderFunction = () => {
+  const data = {}
+
+  return json(data)
+}
+
+/**
+ * Pricing Page
+ */
+export default function Pricing() {
+  ReactGA.send({ hitType: 'pageview', page: '/blog' })
+
+  const data = useLoaderData()
+
+  return (
+    <>
+      <h1>Blog</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
+  )
+}
