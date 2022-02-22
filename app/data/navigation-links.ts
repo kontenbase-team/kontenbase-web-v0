ac1774390bc5ad3306fc5f3f9ae4c76dc4527d8f
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-23 05:35:13 +0700

    feat: navigation links as data

---
+++ app/data/navigation-links.ts
@@ -0,0 +1,11 @@
+export const navigationLinks = [
+  { to: '/', text: 'Home' },
+  { to: '/pricing', text: 'Pricing' },
+  { to: '/contact', text: 'Contact' },
+  { to: '/blog', text: 'Blog' },
+  { to: '/learn', text: 'Learn' },
+  { to: '/community', text: 'Community' },
+  { to: '/showcase', text: 'Showcase' },
+  { to: '/docs', text: 'Docs' },
+  { to: '/jobs', text: 'Jobs' },
+]
